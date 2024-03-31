df20537aa90be53e82fb690cc7490f4aaa0d7d09
Eliam1731 <eliam1731@hotmail.com>
2024-03-30 20:34:01 -0600

    Creacion de php work y Validacion de codigo OPCIC

--- server/insert/equipment.php
+++ server/insert/equipment.php
@@ -23,7 +23,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     ];
 
      // Si el código es una cadena vacía, establecerlo en null
-     if ($data['codigo'] === '') {
+    if ($data['codigo'] === '') {
         $data['codigo'] = null;
     }
 
@@ -42,7 +42,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     if ($row) {
         $response['status'] = 'error';
-        $response['message'] = 'El número de serie ya existe. Por favor, introduce otro.';
+        $response['message'] = 'El número de serie ya está en uso. Intenta con otro número de serie.';
     } else {
         // Verificar si la dirección MAC ya existe
         $stmt = $conn->prepare("SELECT Direccion_mac_wifi FROM equipos_informaticos WHERE Direccion_mac_wifi = :addressMacWifi");
@@ -52,7 +52,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
         if ($row) {
             $response['status'] = 'error';
-            $response['message'] = 'La dirección MAC ya existe. Por favor, introduce otra.';
+            $response['message'] = 'La dirección MAC WIFI ya está en uso. Intenta con otra dirección MAC WIFI.';
         } else {
             // Verificar si el número de referencia de Compaq ya existe
             $stmt = $conn->prepare("SELECT Num_ref_compaq FROM equipos_informaticos WHERE Num_ref_compaq = :referenceCompaq");
@@ -62,7 +62,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
             if ($row) {
                 $response['status'] = 'error';
-                $response['message'] = 'El número de referencia de Compaq ya existe. Por favor, introduce otro.';
+                $response['message'] = 'El número de referencia de Compaq ya está en uso. Intenta con otro número de referencia de Compaq.';
             } else {
                 // Verificar si el Service tag ya existe
                 $stmt = $conn->prepare("SELECT Service_tag FROM equipos_informaticos WHERE Service_tag = :serviceTag");
@@ -72,95 +72,109 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
                 if ($row) {
                     $response['status'] = 'error';
-                    $response['message'] = 'El Service tag del equipo ya existe. Por favor, introduce otro.';
+                    $response['message'] = 'El Service tag del equipo ya está en uso. Intenta con otro Service tag.';
                 } else {
-                    // Si todas las verificaciones son exitosas, entonces insertar los datos
-                    $conn->beginTransaction();
-                    $equipment_id = null;
+                    //Aqui realice la validacion del Codigo OPCIC espero este bien:(
+                    $stmt = $conn->prepare("SELECT miId FROM equipos_informaticos WHERE miId = :codigo");
+                    $stmt->bindParam(':codigo', $data['codigo']);
+                    $stmt->execute();
+                    $row = $stmt->fetch(PDO::FETCH_ASSOC);
 
-                    try {
-                        $stmt = $conn->prepare("INSERT INTO equipos_informaticos (
-                            Id_subcategoria,
-                            Id_marca,
-                            Modelo,
-                            Num_serie,
-                            Especificacion,
-                            Fecha_compra,
-                            Fecha_garantia,
-                            Importe,
-                            Direccion_mac_wifi,
-                            Direccion_mac_ethernet,
-                            Num_ref_compaq,
-                            Service_tag,
-                            Comentarios,
-                            Status_id
-                            ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
-
-                        $executeResult = $stmt->execute([
-                            $data['subcategoryId'],
-                            $data['brandDevices'],
-                            $data['modelDevices'],
-                            $data['serialNumber'],
-                            $data['specificationDevices'],
-                            $data['dateBuy'],
-                            $data['dateExpiresWarranty'],
-                            $data['amountDevices'],
-                            $data['addressMacWifi'],
-                            $data['addressEthernet'],
-                            $data['referenceCompaq'],
-                            $data['serviceTag'],
-                            $data['detailsExtraDevices'],
-                            $data['status'],
-                        ]);
-
-                        if (!$executeResult) {
-                            // La consulta falló
-                            $errorInfo = $stmt->errorInfo();
-                            throw new Exception('La consulta de inserción falló: ' . var_export($errorInfo, true));
-                        }
-
-                        $equipment_id = $conn->lastInsertId();
-
-                        if ($data['codigo'] === null) {
-                            $data['codigo'] = 'OPCIC-COM-' . str_pad($equipment_id, 5, '0', STR_PAD_LEFT);
-                        } else {
-                            $data['codigo'] = 'OPCIC-COM-' . $data['codigo'];
-                        }
-
-                        $stmt = $conn->prepare("UPDATE equipos_informaticos SET miId = ? WHERE Equipo_id = ?");
-                        $stmt->execute([$data['codigo'], $equipment_id]);
-
-                        // Insertar imágenes
-                        for ($i = 0; $i < count($_FILES['images']['name']); $i++) {
-                            $imageName = $_FILES['images']['name'][$i];
-                            $imageType = $_FILES['images']['type'][$i];
-                            $imageData = file_get_contents($_FILES['images']['tmp_name'][$i]);
-
-                            $stmt = $conn->prepare("INSERT INTO imagenes (Nombre, Tipo_mime, Datos_imagen, Equipo_id) VALUES (?, ?, ?, ?)");
-                            $stmt->execute([$imageName, $imageType, $imageData, $equipment_id]);
-                        }
-
-                        // Insertar facturas
-                        if (isset($_FILES['invoices'])) {
-                            for ($i = 0; $i < count($_FILES['invoices']['name']); $i++) {
-                                $invoiceContent = file_get_contents($_FILES['invoices']['tmp_name'][$i]);
-
-                                $stmt = $conn->prepare("INSERT INTO facturas (Factura_file, Equipo_id) VALUES (?, ?)");
-                                $stmt->execute([$invoiceContent, $equipment_id]);
-                            }
-                        }
-
-                        $conn->commit();
-
-                        $response['status'] = 'success';
-                        $response['message'] = 'El registro se realizó correctamente.';
-                        $response['equipment_id'] = $equipment_id;
-                        $response['formatted_equipment_id'] = $data['codigo'];
-                    } catch (Exception $e) {
-                        $conn->rollback();
-
+                    if ($row) {
                         $response['status'] = 'error';
-                        $response['message'] = 'Hubo un error al realizar el registro.' . $e->getMessage();
+                        $response['message'] = 'El código ' . $data['codigo'] . ' ya está en uso. Intenta con otro código.';
+                    } else {
+
+                    // Si todas las verificaciones son exitosas, entonces insertar los datos
+                        $conn->beginTransaction();
+                        $equipment_id = null;
+
+                        try {
+                            $stmt = $conn->prepare("INSERT INTO equipos_informaticos (
+                                Id_subcategoria,
+                                Id_marca,
+                                Modelo,
+                                Num_serie,
+                                Especificacion,
+                                Fecha_compra,
+                                Fecha_garantia,
+                                Importe,
+                                Direccion_mac_wifi,
+                                Direccion_mac_ethernet,
+                                Num_ref_compaq,
+                                Service_tag,
+                                Comentarios,
+                                Status_id
+                                ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+
+                            $executeResult = $stmt->execute([
+                                $data['subcategoryId'],
+                                $data['brandDevices'],
+                                $data['modelDevices'],
+                                $data['serialNumber'],
+                                $data['specificationDevices'],
+                                $data['dateBuy'],
+                                $data['dateExpiresWarranty'],
+                                $data['amountDevices'],
+                                $data['addressMacWifi'],
+                                $data['addressEthernet'],
+                                $data['referenceCompaq'],
+                                $data['serviceTag'],
+                                $data['detailsExtraDevices'],
+                                $data['status'],
+                            ]);
+
+                            if (!$executeResult) {
+                                // La consulta falló
+                                $errorInfo = $stmt->errorInfo();
+                                throw new Exception('La consulta de inserción falló: ' . var_export($errorInfo, true));
+                            }
+
+                            $equipment_id = $conn->lastInsertId();
+
+                            if ($data['codigo'] === null) {
+                                $data['codigo'] = 'OPCIC-COM-' . str_pad($equipment_id, 5, '0', STR_PAD_LEFT);
+                            } else {
+                                $data['codigo'] = 'OPCIC-COM-' . $data['codigo'];
+                            }
+
+                            $stmt = $conn->prepare("UPDATE equipos_informaticos SET miId = ? WHERE Equipo_id = ?");
+                            $stmt->execute([$data['codigo'], $equipment_id]);
+
+                            
+
+                            // Insertar imágenes
+                            for ($i = 0; $i < count($_FILES['images']['name']); $i++) {
+                                $imageName = $_FILES['images']['name'][$i];
+                                $imageType = $_FILES['images']['type'][$i];
+                                $imageData = file_get_contents($_FILES['images']['tmp_name'][$i]);
+
+                                $stmt = $conn->prepare("INSERT INTO imagenes (Nombre, Tipo_mime, Datos_imagen, Equipo_id) VALUES (?, ?, ?, ?)");
+                                $stmt->execute([$imageName, $imageType, $imageData, $equipment_id]);
+                            }
+
+                            // Insertar facturas
+                            if (isset($_FILES['invoices'])) {
+                                for ($i = 0; $i < count($_FILES['invoices']['name']); $i++) {
+                                    $invoiceContent = file_get_contents($_FILES['invoices']['tmp_name'][$i]);
+
+                                    $stmt = $conn->prepare("INSERT INTO facturas (Factura_file, Equipo_id) VALUES (?, ?)");
+                                    $stmt->execute([$invoiceContent, $equipment_id]);
+                                }
+                            }
+
+                            $conn->commit();
+
+                            $response['status'] = 'success';
+                            $response['message'] = 'El registro se realizó correctamente.';
+                            $response['equipment_id'] = $equipment_id;
+                            $response['formatted_equipment_id'] = $data['codigo'];
+                        } catch (Exception $e) {
+                            $conn->rollback();
+
+                            $response['status'] = 'error';
+                            $response['message'] = 'Hubo un error al realizar el registro.' . $e->getMessage();
+                        }
                     }
                 }
             }
--- server/insert/work.php
+++ server/insert/work.php
@@ -1,4 +1,7 @@
 <?php
+
+    include '../config/connection_db.php';
+
     $json = file_get_contents('php://input');
     $data = json_decode($json, true);
 
@@ -11,16 +14,72 @@
     $groupForehead = $data['groupForeheads'];
 
     //APARTIR DE AQUI PUEDES BORRAR TODO Y EMPEZAR A HACERLO FUNCIONAL, SOLO PUSE ESTO PARA TESTEAR
-    $response = [
-        'nameWork' => $nameWork,
-        'shortNameWork' => $shortNameWork,
-        'numWork' => $numWork,
-        'groupCompany' => $groupCompany,
-        'groupForeheads' => $groupForehead,
-        'message' => 'Los datos se recibieron correctamente',
+    $messages = [
+        "workExisting" => 'No es posible registrar esta obra porque ya está en uso. Intenta con otro nombre.',
+        "shortNameWork" => 'No es posible registrar el nombre corto de la obra porque ya está en uso. Intenta con otro nombre.',
+        "numWork" => 'No es posible registrar el número de obra porque ya está en uso. Intenta con otro número.',
+        "succesful" => '¡Felicidades! Tu registro se ha realizado correctamente.',
+        "failed" => 'Ha ocurrido un error al registrar la obra. Por favor, inténtalo más tarde.'
     ];
 
-    $jsonResponse = json_encode($response);
-    header('Content-Type: application/json');
-    echo $jsonResponse;
+    if($_SERVER['REQUEST_METHOD'] === 'POST') {
+        $json = file_get_contents('php://input');
+        $obj = json_decode($json);
+        $data = [
+            "nameWork" => $obj->nameWork,
+            "shortNameWork" => $obj->shortNameWork,
+            "numWork" => $obj->numWork,
+            "groupCompany" => $obj->groupCompany,
+            "groupForeheads" => $obj->groupForeheads
+        ];
+
+        // Verificar si el nombre de la obra ya existe ya se me esta haciendo mas easy
+        $stmt = $conn->prepare("SELECT * FROM obras WHERE Nombre_obra = ?");
+        $stmt->execute([$data["nameWork"]]);
+        if($stmt->fetch()) {
+            header('Content-Type: application/json');
+            echo json_encode(["error" => "workExisting", "message" => $messages["workExisting"]]);
+            exit;
+        }
+
+        // Verificar si el nombre corto de la obra ya existe
+        $stmt = $conn->prepare("SELECT * FROM obras WHERE Nom_corto_obra = ?");
+        $stmt->execute([$data["shortNameWork"]]);
+        if($stmt->fetch()) {
+            header('Content-Type: application/json');
+            echo json_encode(["error" => "shortNameWork", "message" => $messages["shortNameWork"]]);
+            exit;
+        }
+
+        // Verificar si el número de obra ya existe
+        $stmt = $conn->prepare("SELECT * FROM obras WHERE Num_obra = ?");
+        $stmt->execute([$data["numWork"]]);
+        if($stmt->fetch()) {
+            header('Content-Type: application/json');
+            echo json_encode(["error" => "numWork", "message" => $messages["numWork"]]);
+            exit;
+        }
+
+        try {
+            // Insertar datos en la tabla obras
+            $insertData = $conn->prepare("INSERT INTO obras (Nombre_obra, Nom_corto_obra, Num_obra, Empresa_id, Frente_id) VALUES (?, ?, ?, ?, ?)");
+            foreach($data["groupCompany"] as $company) {
+                foreach($data["groupForeheads"] as $forehead) {
+                    $insertData->execute([
+                        $data["nameWork"],
+                        $data["shortNameWork"],
+                        $data["numWork"],
+                        $company,
+                        $forehead
+                    ]);
+                }
+            }
+
+            header('Content-Type: application/json');
+            print json_encode($messages["succesful"]);
+        } catch(Exception $error) {
+            header('Content-Type: application/json');
+            print json_encode($messages['failed'], $error->getMessage());
+        }
+    }
 ?>

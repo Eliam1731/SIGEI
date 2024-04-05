23abb0674ff4f66c4014fed0d410b96ca7768ed3
Eliam1731 <eliam1731@hotmail.com>
2024-04-05 08:24:34 -0600

    resguardo y devolucion de equipo php

--- server/insert/resguard.php
+++ server/insert/resguard.php
@@ -4,24 +4,48 @@ include '../config/connection_db.php';
 $data = json_decode(file_get_contents('php://input'), true);
 
 $equipments = $data['equipments'];
-$employee = intval($data['employee']); // Convierte a entero para no tener problemas
-$date_auth = $data['date_auth'];
-$user = intval($data['user']); // Convierte a entero para no tenr problemas igual que lo anterior
+$employee = intval($data['employee']); 
+$user = $data['user']; 
 $observation = $data['observation'];
 
-// Prepara la consulta SQL para insertar el resguardos 
+
+$sqlEmployee = "SELECT Correo_electronico FROM empleados_resguardantes WHERE Empleado_id = :employee_id";
+$stmtEmployee = $conn->prepare($sqlEmployee);
+$stmtEmployee->execute([':employee_id' => $employee]);
+
+
+if ($stmtEmployee->rowCount() == 0) {
+    echo json_encode(["message" => "El empleado no existe en la base de datos"]);
+    exit();
+}
+
+
+$employeeEmail = $stmtEmployee->fetch(PDO::FETCH_ASSOC)['Correo_electronico'];
+
+
+$sqlCheck = "SELECT * FROM usuarios WHERE correo_electronico = :email";
+$stmtCheck = $conn->prepare($sqlCheck);
+$stmtCheck->execute([':email' => $user]);
+
+
+if ($stmtCheck->rowCount() == 0) {
+    echo json_encode(["message" => "El correo electrónico no existe en la base de datos"]);
+    exit();
+}
+
+
 $sql = "INSERT INTO resguardos_de_equipos (Equipo_id, Empleado_id, Fecha_autorizacion, User_id, Comentario, Identificador_resguardo) VALUES (:equipment, :employee, :date_auth, :user, :observation, :resguardo_id)";
 $stmt = $conn->prepare($sql);
 
-// Prepara la consulta SQL para actualizar el status
+
 $sqlUpdate = "UPDATE equipos_informaticos SET Status_id = 2 WHERE Equipo_id = :equipment";
 $stmtUpdate = $conn->prepare($sqlUpdate);
 
 $messages = [];
 
-// Ejecuta las consultas para cada equipo
+
 foreach ($equipments as $equipment) {
-    // Genera un identificador único de resguardo
+
     $resguardo_id = uniqid();
 
     // Vincula los parámetros y ejecuta la consulta de inserción
@@ -31,5 +55,5 @@ foreach ($equipments as $equipment) {
     $stmtUpdate->execute([':equipment' => intval($equipment)]);
 }
 
-echo json_encode(["message" => "Su resguardo fue exitoso"]);
+echo json_encode(["message" => "Su resguardo fue exitoso", "employeeEmail" => $employeeEmail]);
 ?>

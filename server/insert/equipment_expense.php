2bdd1b78d1f6a02313b34c5e9213e2b9ea9dbd71
Eliam1731 <eliam1731@hotmail.com>
2024-04-26 00:15:49 -0600

    correccion gastos equipo

--- server/insert/equipment_expense.php
+++ server/insert/equipment_expense.php
@@ -2,6 +2,15 @@
 include '../config/connection_db.php';
 
 try {
+    if (!isset($_POST['Equipo_id'])) {
+        throw new Exception("Equipo_id no puede ser nulo");
+    }
+
+    if (!isset($_FILES['Recibo_pdf'])) {
+        throw new Exception("Recibo_pdf no puede ser nulo");
+    }
+    
+    $reciboContent = file_get_contents($_FILES['Recibo_pdf']['tmp_name']);
 
     $stmt = $conn->prepare("INSERT INTO gastos_de_los_equipos (Equipo_id, Cantidad, Piezas, Importe, Fecha, Recibo_pdf) VALUES (:Equipo_id, :Cantidad, :Piezas, :Importe, :Fecha, :Recibo_pdf)");
 
@@ -10,12 +19,14 @@ try {
     $stmt->bindParam(':Piezas', $_POST['Piezas']);
     $stmt->bindParam(':Importe', $_POST['Importe']);
     $stmt->bindParam(':Fecha', $_POST['Fecha']);
-    $stmt->bindParam(':Recibo_pdf', $_POST['Recibo_pdf']);
+    $stmt->bindParam(':Recibo_pdf', $reciboContent, PDO::PARAM_LOB);
 
     $stmt->execute();
 
     echo "Los gastos fueron subidos con éxito";
 } catch(PDOException $e) {
+    echo "Error: " . $e->getMessage();
+} catch(Exception $e) {
     echo "Error: " . $e->getMessage();
 }
 ?>

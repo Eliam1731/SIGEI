e42b50c5e0f10ca35378b29f93753cc20b7c2869
Eliam1731 <eliam1731@hotmail.com>
2024-05-12 12:42:48 -0600

    mantenimiento preventivo inicio y terminacion corregido

--- server/data/finish_maintenance.php
+++ server/data/finish_maintenance.php
@@ -1,31 +1,30 @@
 <?php
 include '../config/connection_db.php';
 
-// Recibe el JSON del front-end
 $data = json_decode(file_get_contents('php://input'), true);
 
 try {
-    // Inicia una transacción
+    
     $conn->beginTransaction();
 
-    // Prepara la consulta SQL para actualizar la tabla mantenimiento
+    
     $sql = "UPDATE mantenimiento SET termino_mante = ? WHERE Equipo_id = ?";
     $stmt = $conn->prepare($sql);
     $stmt->execute([$data['termino_mante'], $data['Equipo_id']]);
 
-    // Prepara la consulta SQL para actualizar el status en la tabla equipos_informaticos
+    
     $sql = "UPDATE equipos_informaticos SET Status_id = 1 WHERE Equipo_id = ?";
     $stmt = $conn->prepare($sql);
     $stmt->execute([$data['Equipo_id']]);
 
-    // Confirma las operaciones
+    
     $conn->commit();
 
-    // Devuelve un JSON con el mensaje de éxito
+    
     header('Content-Type: application/json');
     echo json_encode(['message' => 'El equipo está disponible']);
 } catch (PDOException $e) {
-    // Si algo sale mal, revierte las operaciones
+    
     $conn->rollBack();
     header('Content-Type: application/json');
     echo json_encode(['message' => 'Error: ' . $e->getMessage()]);
--- server/insert/maintenance_equipment.php
+++ server/insert/maintenance_equipment.php
@@ -1,31 +1,29 @@
 <?php
 include '../config/connection_db.php';
 
-// Recibe el JSON del front-end
 $data = json_decode(file_get_contents('php://input'), true);
 
 try {
-    // Inicia una transacción
+    
     $conn->beginTransaction();
 
-    // Prepara la consulta SQL para insertar en la tabla mantenimiento
+    
     $sql = "INSERT INTO mantenimiento (Equipo_id, User_id, inicio_mante, Motivo_mantenimiento) VALUES (?, ?, ?, ?)";
     $stmt = $conn->prepare($sql);
     $stmt->execute([$data['Equipo_id'], $data['User_id'], $data['inicio_mante'], $data['Motivo_mantenimiento']]);
 
-    // Prepara la consulta SQL para actualizar el status en la tabla equipos_informaticos
     $sql = "UPDATE equipos_informaticos SET Status_id = 3 WHERE Equipo_id = ?";
     $stmt = $conn->prepare($sql);
     $stmt->execute([$data['Equipo_id']]);
 
-    // Confirma las operaciones
+
     $conn->commit();
 
-    // Devuelve un JSON con el mensaje de éxito
+
     header('Content-Type: application/json');
     echo json_encode(['message' => 'El equipo se trasladó a mantenimiento']);
 } catch (PDOException $e) {
-    // Si algo sale mal, revierte las operaciones
+
     $conn->rollBack();
     header('Content-Type: application/json');
     echo json_encode(['message' => 'Error: ' . $e->getMessage()]);

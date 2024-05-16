1b5b965d83eaa6699fb6944e1c9b041bc8701e3a
Eliam1731 <eliam1731@hotmail.com>
2024-05-16 16:02:52 -0600

    inicio y termino del mantenimiento

--- server/data/finish_maintenance.php
+++ server/data/finish_maintenance.php
@@ -7,22 +7,29 @@ try {
     
     $conn->beginTransaction();
 
-    
-    $sql = "UPDATE mantenimiento SET termino_mante = ? WHERE Equipo_id = ?";
+    $sql = "UPDATE mantenimiento SET termino_mante = ?, hora_termino = ?, Motivo_mantenimiento = ? WHERE Equipo_id = ?";
     $stmt = $conn->prepare($sql);
-    $stmt->execute([$data['termino_mante'], $data['Equipo_id']]);
+    $stmt->execute([$data['termino_mante'], $data['hora_termino'], $data['Motivo_mantenimiento'], $data['Equipo_id']]);
 
-    
+
     $sql = "UPDATE equipos_informaticos SET Status_id = 1 WHERE Equipo_id = ?";
     $stmt = $conn->prepare($sql);
     $stmt->execute([$data['Equipo_id']]);
 
-    
+
+    $sql = "SELECT * FROM mantenimiento WHERE Equipo_id = ?";
+    $stmt = $conn->prepare($sql);
+    $stmt->execute([$data['Equipo_id']]);
+    $equipo = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    $equipo['horaInicio'] = date('g:i A', strtotime($equipo['horaInicio']));
+    $equipo['hora_termino'] = date('g:i A', strtotime($equipo['hora_termino']));
+
     $conn->commit();
 
-    
+
     header('Content-Type: application/json');
-    echo json_encode(['message' => 'El equipo está disponible']);
+    echo json_encode(['message' => 'El equipo está disponible', 'equipo' => $equipo]);
 } catch (PDOException $e) {
     
     $conn->rollBack();

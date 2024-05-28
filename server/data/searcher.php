4393878fdfad1e8b741065f7499e99bfe4ca01f3
Eliam1731 <eliam1731@hotmail.com>
2024-05-28 11:34:41 -0600

    Buscador de empleados

---
+++ server/data/searcher.php
@@ -0,0 +1,25 @@
+<?php
+include '../config/connection_db.php';
+
+$data = json_decode(file_get_contents('php://input'), true);
+$search = $data['search'];
+
+$sql_search = "SELECT * FROM empleados_resguardantes WHERE Nombre LIKE :search OR Primer_apellido LIKE :search OR Segundo_apellido LIKE :search";
+$stmt_search = $conn->prepare($sql_search);
+
+try {
+    $stmt_search->execute(['search' => "%$search%"]);
+    $results = $stmt_search->fetchAll(PDO::FETCH_ASSOC);
+    echo json_encode($results);
+} catch (PDOException $e) {
+    echo json_encode([
+        "message" => "Hubo un error en la búsqueda de empleados", 
+        "error" => [
+            "code" => $e->getCode(),
+            "message" => $e->getMessage(),
+            "file" => $e->getFile(),
+            "line" => $e->getLine()
+        ]
+    ]);
+}
+?>

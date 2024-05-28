86d885f49d9d61f036d27b3983c711834cf293ca
Eliam1731 <eliam1731@hotmail.com>
2024-05-28 10:39:10 -0600

    Update Imagen

--- server/insert/update_img.php
+++ server/insert/update_img.php
@@ -1,13 +1,7 @@
 <?php
 include '../config/connection_db.php';
 
-$data = json_decode(file_get_contents('php://input'), true);
-
-$equipo_id = $data['equipo_id'];
-$new_images = $data['new_images']; //'new_images' array de archivos
-
-$sql_delete = "DELETE FROM imagenes WHERE Equipo_id = :equipo_id";
-$stmt_delete = $conn->prepare($sql_delete);
+$equipo_id = $_POST['equipo_id'];
 
 $sql_insert = "INSERT INTO imagenes (Nombre, Tipo_mime, Datos_imagen, Equipo_id) VALUES (:nombre, :tipo_mime, :datos_imagen, :equipo_id)";
 $stmt_insert = $conn->prepare($sql_insert);
@@ -15,17 +9,13 @@ $stmt_insert = $conn->prepare($sql_insert);
 try {
     $conn->beginTransaction();
 
-    // Eliminar imágenes 
-    $stmt_delete->execute(['equipo_id' => $equipo_id]);
-
-    // Insertar nuevas imágenes
-    foreach ($new_images as $image) {
-        $imageData = file_get_contents($image['tmp_name']);
+    foreach ($_FILES['new_images']['tmp_name'] as $key => $tmp_name) {
+        $imageData = file_get_contents($tmp_name);
         $finfo = new finfo(FILEINFO_MIME_TYPE);
-        $mime_type = $finfo->file($image['tmp_name']);
+        $mime_type = $finfo->file($tmp_name);
 
         $stmt_insert->execute([
-            'nombre' => $image['name'],
+            'nombre' => $_FILES['new_images']['name'][$key],
             'tipo_mime' => $mime_type,
             'datos_imagen' => $imageData,
             'equipo_id' => $equipo_id
@@ -33,11 +23,11 @@ try {
     }
 
     $conn->commit();
-    echo json_encode(["message" => "Las imágenes se actualizaron exitosamente"]);
+    echo json_encode(["message" => "Las imágenes se insertaron exitosamente"]);
 } catch (PDOException $e) {
     $conn->rollBack();
     echo json_encode([
-        "message" => "Hubo un error en la actualización de las imágenes", 
+        "message" => "Hubo un error en la inserción de las imágenes", 
         "error" => [
             "code" => $e->getCode(),
             "message" => $e->getMessage(),

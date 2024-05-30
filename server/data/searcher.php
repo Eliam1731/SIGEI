ec32fa75b038ac76a7fa64d961fcc81ad25e7862
Eliam1731 <eliam1731@hotmail.com>
2024-05-30 11:28:35 -0600

    Busqueda de empleado

--- server/data/searcher.php
+++ server/data/searcher.php
@@ -4,11 +4,25 @@ include '../config/connection_db.php';
 $data = json_decode(file_get_contents('php://input'), true);
 $search = $data['search'];
 
-$sql_search = "SELECT * FROM empleados_resguardantes WHERE Nombre LIKE :search OR Primer_apellido LIKE :search OR Segundo_apellido LIKE :search";
+$searchWords = explode(' ', $search);
+
+$searchConditions = [];
+foreach ($searchWords as $word) {
+    $searchConditions[] = "CONCAT(Nombre, ' ', Primer_apellido, ' ', Segundo_apellido) LIKE :$word";
+}
+
+
+$sql_search = "SELECT * FROM empleados_resguardantes WHERE " . implode(' OR ', $searchConditions);
 $stmt_search = $conn->prepare($sql_search);
 
+
+$searchParams = [];
+foreach ($searchWords as $word) {
+    $searchParams[$word] = "%$word%";
+}
+
 try {
-    $stmt_search->execute(['search' => "%$search%"]);
+    $stmt_search->execute($searchParams);
     $results = $stmt_search->fetchAll(PDO::FETCH_ASSOC);
     echo json_encode($results);
 } catch (PDOException $e) {

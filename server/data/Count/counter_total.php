0afdfedbf33802fa727b6479e9afa747bed29afc
Eliam1731 <eliam1731@hotmail.com>
2024-05-31 10:29:12 -0600

    Correccion de cuenta total sin los de baja

--- server/data/Count/counter_total.php
+++ server/data/Count/counter_total.php
@@ -2,11 +2,12 @@
 
 include '../../config/connection_db.php';
 
-$sql = $conn->prepare("SELECT COUNT(*) AS total FROM equipos_informaticos ");
+$sql = $conn->prepare("SELECT COUNT(*) AS total FROM equipos_informaticos WHERE Status_id != 4");
 $sql->execute();
 
 $result = $sql->fetch();
 $response = array("total"=> $result['total']);
 
 echo json_encode($response);
+
 ?>

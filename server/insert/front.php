fdd958f897c17c9109c0fb644d1a836f3288dfee
Eliam1731 <eliam1731@hotmail.com>
2024-06-04 17:36:28 -0600

    crear frentes

---
+++ server/insert/front.php
@@ -0,0 +1,32 @@
+<?php
+include '../config/connection_db.php';
+
+header('Content-Type: application/json');
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    
+    // Decodifica el JSON recibido
+    $data = json_decode(file_get_contents('php://input'), true);
+    $nombre_frente = $data['nombre_frente'];
+    $numero_frente = $data['numero_frente'];
+
+    $stmt = $conn->prepare("SELECT * FROM frente WHERE Nom_frente = :nombre_frente OR numero_frente = :numero_frente");
+    $stmt->execute(['nombre_frente' => $nombre_frente, 'numero_frente' => $numero_frente]);
+    $row = $stmt->fetch();
+
+    if ($row) {
+        http_response_code(400);
+        echo json_encode(['error' => 'Ese frente ya existe.']);
+    } else {
+        $stmt = $conn->prepare("INSERT INTO frente (Nom_frente, numero_frente) VALUES (:nombre_frente, :numero_frente)");
+        $stmt->execute(['nombre_frente' => $nombre_frente, 'numero_frente' => $numero_frente]);
+
+        if ($stmt->rowCount() > 0) {
+            echo json_encode(['message' => 'Frente creado exitosamente.']);
+        } else {
+            http_response_code(500);
+            echo json_encode(['error' => 'No se pudo crear el frente.']);
+        }
+    }
+}
+?>

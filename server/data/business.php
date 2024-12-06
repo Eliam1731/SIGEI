e932109def65f03e2dff5e8e01096c4a90141a6e
Eliam1731 <eliam1731@hotmail.com>
2024-12-06 15:07:41 -0600

    Numero a obra y frente

--- server/data/business.php
+++ server/data/business.php
@@ -1,24 +1,26 @@
 <?php
     include '../config/connection_db.php';
 
-    
     $data = [
         'work' => [],
         'forehead' => [],
         'company' => []
     ];
 
-    $sql = "SELECT Obra_id, Nombre_obra FROM obras ORDER BY Obra_id ASC";
+
+    $sql = "SELECT Obra_id, Nombre_obra, Num_obra FROM obras ORDER BY Obra_id ASC";
     $stmt = $conn->query($sql);
     while($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-        $data['work'][] = [$row['Obra_id'], $row['Nombre_obra']];
+        $data['work'][] = [$row['Obra_id'], $row['Nombre_obra'] . ' (' . $row['Num_obra'] . ')'];
     }
 
-    $sql = "SELECT Frente_id, Nom_frente FROM frente ORDER BY Frente_id ASC";
+
+    $sql = "SELECT Frente_id, Nom_frente, numero_frente FROM frente ORDER BY Frente_id ASC";
     $stmt = $conn->query($sql);
     while($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-        $data['forehead'][] = [$row['Frente_id'], $row['Nom_frente']];
+        $data['forehead'][] = [$row['Frente_id'], $row['Nom_frente'] . ' (' . $row['numero_frente'] . ')'];
     }
+
 
     $sql = "SELECT Empresa_id, Nom_empresa FROM empresas ORDER BY Empresa_id ASC";
     $stmt = $conn->query($sql);
